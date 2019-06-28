577efd31a69eb4fcfa849bebe2d4ab23f05c6bc2
cuberwr <cuberwr@gmail.com>
2019-06-29 06:40:44 +0800

    修改了实现逻辑

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-//获取本机外网IP
 const request = require('request');
 
-var ip = '';
-
-function getIP() {
-    request('http://ip.6655.com/ip.aspx?area=1', (err, res, body) => {
-        ip = body.substring(0, body.indexOf('|'));
-    });
-}
-
-
-//阿里云API修改DNS解析
 const Core = require('@alicloud/pop-core');
 
-//填入你的id和key，在阿里云控制台查询
+//填写id/key
 var client = new Core({
-    accessKeyId: '',
-    accessKeySecret: '',
+    accessKeyId: '',//id
+    accessKeySecret: '',//key
     endpoint: 'https://alidns.aliyuncs.com',
     apiVersion: '2015-01-09'
 });
 
-//以下信息需完成
+//填写修改信息
 var params = {
     "RecordId": "",
     "RR": "",
     "Type": "",
-    "Value": '', //非必须
+    "Value": '',//非必须
 };
 
 var requestOption = {
@@ -34,14 +23,19 @@ var requestOption = {
 };
 
 function check_change() {
-    if (!(ip === params.Value)||!params.Value) {
-        params.Value = ip;
-        client.request('UpdateDomainRecord', params, requestOption).then((result) => {
-            console.log(JSON.stringify(result));
-        }, (ex) => {
-            console.log(ex);
-        });
-    }else{console.log('no change')}
+    request('http://ip.6655.com/ip.aspx?area=1', (err, res, body) => {
+        var ip = body.substring(0, body.indexOf('|'));
+        if (params.Value !=ip) {
+            params.Value = ip;
+            client.request('UpdateDomainRecord', params, requestOption).then((result) => {
+                console.log(JSON.stringify(result));
+            }, (ex) => {
+                console.log(ex);
+            });
+        }else{
+            console.log('no change');
+        }
+    });   
 }
 
 check_change();
